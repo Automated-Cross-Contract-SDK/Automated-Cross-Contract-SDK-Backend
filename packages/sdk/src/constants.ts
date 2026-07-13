c4fa29d7e799d10a0b5aeed005e7061c1ec0a855
Michael Musa <micmusjnr20@gmail.com>
2026-07-13 19:01:39 +0000

    refactor: extract magic numbers into constants module

---
+++ packages/sdk/src/constants.ts
@@ -0,0 +1,6 @@
+export const MAX_XDR_SIZE_BYTES = 100_000
+export const DEFAULT_RESTORE_FEE = '100000'
+export const MAX_RETRIES = 3
+export const RETRY_DELAY_MS = 500
+export const DEFAULT_POLL_ATTEMPTS = 30
+export const POLL_INTERVAL_MS = 1000
